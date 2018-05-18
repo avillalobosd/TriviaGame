b73c3df222a7a3dac33ce54580f19f81cce4ad35
avillalobosd <lalovide@gmail.com>
2018-05-18 17:42:33 -0500

    v2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,11 +7,12 @@ var pregunta5 = ["Da click en el numero cinco", "1", "2", "5", "4"];
 var pregunta6 = ["Da click en el numero seis", "1", "6", "3", "4"];
 var pregunta7 = ["Da click en el numero siete", "7", "2", "3", "4"];
 var seconds = 5;
-var nextsec=3;
+var nextsec = 3;
 var intervalId;
-var correcta=0;
-var incorrecta=0;
-var paso=1;
+var correcta = 0;
+var incorrecta = 0;
+var paso = 1;
+var trash=0;
 
 $(".startbtn").click(function () {
     $(".buttons1").css("display", "none");
@@ -23,20 +24,20 @@ $(".startbtn").click(function () {
     $(".a4").text(window['pregunta' + pregunta][4]);
     $("#remain").text("Remain time 5");
     function run() {
-        if(paso==1){
+        if (paso == 1) {
             $(".s2").css("display", "block");
             $(".q1").text(window['pregunta' + pregunta][0]);
             $(".a1").text(window['pregunta' + pregunta][1]);
             $(".a2").text(window['pregunta' + pregunta][2]);
             $(".a3").text(window['pregunta' + pregunta][3]);
             $(".a4").text(window['pregunta' + pregunta][4]);
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
         }
-        else if (paso==2){
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement2, 1000);
-        
+        else if (paso == 2) {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement2, 1000);
+
         }
     }
     function decrement2() {
@@ -46,53 +47,59 @@ $(".startbtn").click(function () {
             $(".t1").css("display", "none");
             $(".r1").css("display", "none");
             $(".s2").css("display", "block");
-            nextsec=3;
-            seconds=5;
-            paso=1;
-            $("#remain").text("Remain time "+seconds);
+            nextsec = 3;
+            seconds = 5;
+            paso = 1;
+            $("#remain").text("Remain time " + seconds);
             run();
-        
+
         }
         function stop() {
             clearInterval(intervalId);
         }
 
     }
-  
+
 
 
     function decrement() {
         seconds--;
         $("#remain").text("Remain time " + seconds);
-        
-        if (seconds === 0) {
+
+        if (seconds == 0) {
             stop();
             $(".s2").css("display", "none");
             $(".t1").css("display", "block");
-            $(".correctanswer").text("Correct Answer was: "+ window['pregunta' + pregunta][1]);
+            $(".correctanswer").text("Correct Answer was: " + window['pregunta' + pregunta][1]);
             pregunta++;
-            paso=2;
-            seconds=5;
+            paso = 2;
+            seconds = 5;
+            incorrecta++;
             run();
+        
         }
         function stop() {
             clearInterval(intervalId);
         }
-
-        $(".a1").click(function () {
-            stop();
-            $(".s2").css("display", "none");
-            $(".r1").css("display", "block");
-            $(".correctanswer").text("Correct Answer was: "+ window['pregunta' + pregunta][1]);
-            pregunta++;
-            seconds=5;
-            paso=2;
-            run();
-            
-        });
     }
-  
 
+    $(".a1").click(function () {
+        stop();
+        $(".s2").css("display", "none");
+        $(".r1").css("display", "block");
+        if (pregunta == 1 || pregunta == 7) {
+            $(".correctanswer").text("Well Done");
+            correcta++;
+        } else {
+            $(".correctanswer").text("Correct Answer was: " + window['pregunta' + pregunta][1]);
+            incorrecta++;
+        }
+        pregunta++;
+        seconds = 5;
+        paso = 2;
+        run();
+
+    });
 
 
 
